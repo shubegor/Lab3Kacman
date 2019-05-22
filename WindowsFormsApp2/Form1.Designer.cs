c2f318bc9fff3f596ac4ce6a8aa5db4daffa3b2b
EGOR-ROG\Egor <shubegor@gmail.com>
2019-05-22 21:43:26 +0700

    4 лаба

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -33,9 +33,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(467, 72);
+            this.button1.Location = new System.Drawing.Point(350, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "ТАП";
             this.button1.UseVisualStyleBackColor = true;
@@ -43,12 +44,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 466);
+            this.ClientSize = new System.Drawing.Size(464, 379);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -28,19 +28,40 @@ namespace WindowsFormsApp2
         Chart myChart; 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-           
-            Random r = new Random();
 
-            int ModelTime = 1000;
+
+            Random r = new Random(0);
+           
+
+            int ModelTime = 100000;
             int ochered = 0;
 
-            int min = 5;
-            int max = 7;
-            int nextZ = 0;
-            int nextS = 0;
-            bool SerIsFree = true;
+            double totalZ = 1;
 
+            double totalObsluzh1 = 0;
+            double totalObsluzh2 = 0;
+            double TotalLost = 0;
+
+            double totalWorkTime1 = 0;
+            double totalWorkTime2 = 0;
+            double ProcS1 = 0;
+            double ProcS2 = 0;
+
+            //Настройки интервала заявок
+            int minZ = 5;
+            int maxZ = 7;
+
+            //Настройки интервала серверов
+            int minS = 5;
+            int maxS = 7;
+
+
+
+            int nextZ = 0;
+            int nextS1 = 0;
+            int nextS2 = 0;
+            bool Ser1IsFree = true;
+            bool Ser2IsFree = true;
 
 
 
@@ -55,26 +76,54 @@ namespace WindowsFormsApp2
 
             List<int> och = new List<int>();
         
-            nextZ = r.Next(min, max);
+            nextZ = r.Next(minZ, maxZ);
 
             for (int i = 0; i < ModelTime; i++)
             {
                 if (i == nextZ) //новая заявка
                 {
-                    nextZ = i + r.Next(min, max);
+                    nextZ = i + r.Next(minZ, maxZ);
+                    totalZ++;
                     ochered++;
                 }
-                if (nextS == i)
+
+
+                if (!Ser1IsFree) totalWorkTime1++;
+                if (!Ser2IsFree) totalWorkTime2++;
+
+
+                if (nextS1 == i) //свобода серверу 1
                 {
-                    SerIsFree = true;
+                    totalObsluzh1++;
+                    Ser1IsFree = true;
                 }
+
+                if (nextS2 == i) //свобода серверу 2
+                {
+                    totalObsluzh2++;
+                    Ser2IsFree = true;
+                }
+
+
+                
                 if (ochered > 0) //если есть очередь
                 {
-                    if (SerIsFree) //включаем свободный сервер
+
+                    
+                    if (Ser1IsFree) //включаем свободный сервер 1
                     {
-                        nextS = i + r.Next(min, max);
-                        SerIsFree = false;
+                        nextS1 = i + r.Next(minS, maxS);
+                        Ser1IsFree = false;
                         ochered--;
+                    }
+                    else
+                    {
+                        if (Ser2IsFree) //включаем свободный сервер 2
+                        {
+                            nextS2 = i + r.Next(minS, maxS);
+                            Ser2IsFree = false;
+                            ochered--;
+                        }
                     }
 
                 }
@@ -86,7 +135,13 @@ namespace WindowsFormsApp2
             //Добавляем созданный набор точек в Chart
             if (myChart.Series.Count() > 0) myChart.Series.RemoveAt(0);
             myChart.Series.Add(mySeriesOfPoint);
-            
+
+
+            ProcS1 = totalWorkTime1 / ModelTime * 100;
+            ProcS2 = totalWorkTime2 / ModelTime * 100;
+            TotalLost = totalZ - totalObsluzh1 - totalObsluzh2 - ochered;
         }
+
+        
     }
 }
